a411fdcfff298987fb37affb6b0cc7d19b927393
IVAN <prg938@mail.ru>
2023-06-22 15:45:54 +0300

    Update ProjectList.tsx

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -13,19 +13,29 @@ interface ProjectListType {
   projectList: ProjectItem[]
   justifyContainer?: justifyContaierPropType
 }
+enum ProjectID {
+  MarioGame = 'mariogame',
+  ChromeWeather = 'chrome-weather',
+  MasonrylayoutTsxReact = 'masonrylayout-tsx-react'
+}
 const ProjectList: FC<ProjectListType> = ({projectList, justifyContainer = 'flex-start'}) => {
   const blurDataURL = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAQEBAQEBAQEBAQGBgUGBggHBwcHCAwJCQkJCQwTDA4MDA4MExEUEA8QFBEeFxUVFx4iHRsdIiolJSo0MjRERFwBBAQEBAQEBAQEBAYGBQYGCAcHBwcIDAkJCQkJDBMMDgwMDgwTERQQDxAUER4XFRUXHiIdGx0iKiUlKjQyNEREXP/CABEIAAUABQMBIgACEQEDEQH/xAAUAAEAAAAAAAAAAAAAAAAAAAAI/9oACAEBAAAAAD//AP/EABQBAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIQAAAAf//EABQBAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMQAAAAf//EABQQAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQEAAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z'
   const animate = '.1s ease'
   const gap = 15
   const pathProject = '/project/'
   const w100 = {width: '100%', height: 'auto'}
+  const w80 = {width: '80%', height: 'auto'}
+  const imageStyle = {marginTop: '5px', borderRadius: '7px'}
+  const fs20px = {fontSize: '20px'}
+  const ttu = {textTransform: 'uppercase'}
+  
   const list = projectList.map(project => {
     const {id, repoName, shortDesc, previewSize, preview} = project
     const projectHref = pathProject + id
-    if (id === 'mariogame') {
+    if (id as string === ProjectID.MarioGame) {
       return <div key={id} className={styles.project}>
-        <div style={{fontSize: '20px'}}>
-          <div style={{textTransform: 'uppercase'}}>
+        <div style={fs20px}>
+          <div style={ttu}>
             <Link href={projectHref}>{repoName}</Link>
           </div>
         </div>
@@ -36,12 +46,14 @@ const ProjectList: FC<ProjectListType> = ({projectList, justifyContainer = 'flex
         </div>
       </div>
     }
-    if (id === 'chrome-weather' || id === 'masonrylayout-tsx-react') {
-      let imageStyle = {marginTop: '5px'}
-      if (id === 'chrome-weather') {
-        imageStyle = {...imageStyle, ...w100, ...{borderRadius: '7px'}}
+    if (id as string === ProjectID.ChromeWeather || id as string === ProjectID.MasonrylayoutTsxReact) {
+      if (id as string === ProjectID.ChromeWeather) {
+        imageStyle = {...imageStyle, ...w80}
       }
-      return <div key={id} className={styles.project} style={id === 'chrome-weather' ? {overflow: 'hidden', height: '280px'} : {}}>
+      else {
+        imageStyle = {...imageStyle, ...w100}
+      }
+      return <div key={id} className={styles.project}>
         <h3 className={styles.title}>
           <Link href={projectHref}>{repoName}</Link>
         </h3>
